4c7f0a636c8e92c940ed5df894b598c1c82ba758
Sejal Raghorte <107930918+SejalRaghorte@users.noreply.github.com>
2024-08-29 23:48:58 +0530

    Update practice.js

--- Text Editor/practice.js
+++ Text Editor/practice.js
@@ -1,27 +1,6 @@
 
 
 
-// let btn = document.querySelector("button");
-
-// btn.addEventListener("click",function(){
-//     let h3 = document.querySelector("h3");
-//     let randomcolor = generaterandomcolor();
-//     h3.innerText = randomcolor;
-
-//     let div = document.querySelector("div");
-//     div.style.backgroundColor = randomcolor;
-// });
-
-// function generaterandomcolor(){
-//     let red = Math.floor(Math.random() * 255);
-//     let green = Math.floor(Math.random() * 255);
-//     let blue = Math.floor(Math.random() * 255);
-
-//     let color = `rgb(${red}, ${green}, ${blue})`;
-//     return color;
-// };
-
-
 // let h1 = document.querySelector("h1");
 // let h3 = document.querySelector("h3");
 // let p = document.querySelector("p");
